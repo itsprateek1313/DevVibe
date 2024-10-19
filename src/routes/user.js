5feb764736ec429c050daf1f7c38e358c689b5d9
Prateek Srivastava <113616216+itsprateek1313@users.noreply.github.com>
2024-10-19 20:54:40 +0530

    Backend Finalized

--- src/routes/user.js
+++ src/routes/user.js
@@ -83,7 +83,7 @@ userRouter.get("/feed", userAuth, async (req, res) => {
       .select(USER_SAFE_DATA)
       .skip(skip)
       .limit(limit);
-    res.send(users);
+    res.json({ data: users });
   } catch (error) {
     res.status(400).send(`ERROR:${error.message}`);
   }
